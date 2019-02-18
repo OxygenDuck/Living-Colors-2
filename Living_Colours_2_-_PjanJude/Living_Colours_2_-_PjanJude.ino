087a93d0a61cd80f94da4ad61e328a62214d3fab
Unknown <78942@rocteraa-student.nl>
2019-02-18 14:16:00 +0100

    Creation Template

---
+++ Living_Colours_2_-_PjanJude/Living_Colours_2_-_PjanJude.ino
@@ -0,0 +1,44 @@
+//Created: Jeremey van Uden, 11feb2019, v0
+//Version
+//v0, 11feb2019: initial version
+
+//globals
+bool enableDebugLog = true;
+
+void setup() {
+  // put your setup code here, to run once:
+    InitSerial();
+    InitCreator();
+    InitGlobals();
+}
+
+void InitSerial()
+{
+  Serial.begin(115200);
+  Serial.println("READY: serial initialized");
+}
+
+void InitCreator()
+{
+  Serial.println("Created: Jeremey van Uden, 11feb2019, v0");
+}
+
+void InitGlobals()
+{
+  Serial.println("INIT: globals");
+  enableDebugLog = true;
+  Serial.println("READY: globals initialized");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+void PrintLn(String a_inputString)
+{
+  if(enableDebugLog == true)
+  {
+    Serial.println(a_inputString);
+  }
+}
+
