44128eeeb1330d59df92e0f092ca727eb9004391
Unknown <78942@rocteraa-student.nl>
2019-02-18 15:45:15 +0100

    Added Demo

--- Living_Colours_2_-_PjanJude/Living_Colours_2_-_PjanJude.ino
+++ Living_Colours_2_-_PjanJude/Living_Colours_2_-_PjanJude.ino
@@ -4,6 +4,17 @@
 
 //globals
 bool enableDebugLog = true;
+int colorNr = 0;
+int colorValue = 0;
+#define color_Delay 100
+//io
+#define doLedTest 6
+#define diPresetRGB 7
+#define diPresetCMY 8
+#define aoRed 9
+#define aoGreen 11
+#define aiPotMeter A0
+
 void setup() {
   // put your setup code here, to run once:
     InitSerial();
@@ -28,12 +39,56 @@ void InitGlobals()
   enableDebugLog = true;
   Serial.println("READY: globals initialized");
 }
+
+void InitDemo()
+{
+  Serial.println("INIT: Demo");
+  //loop to handle three colors
+  while(colorNr < 3)
+  {
+   while(colorValue < 256)
+  {
+    //loop to handle one color incrementing
+    UpdateColor();
+    colorValue--;
+  }
+  while(colorValue >= 0)
+  {
+    //loop to handle one color incrementing
+    UpdateColor();
+    colorValue++;
+  }
+  colorNr++;
+  }
+  
+  //inside
+  //loop to handle one color increment
+  Serial.println("READY: Demo");
+}
+
+void UpdateColor()
+{
+  switch(colorNr)
+  {
+      case 0:
+        analogWrite(aoRed, colorValue);
+       break;
+      case 1:
+        analogWrite(aoGreen, colorValue);
+       break;
+      case 2:
+        analogWrite(aoBlue, colorValue);
+       break;
+  }
+  PrintLn("Colorvalue : " + (String)colorvalue + " > colorNr : " + (String)colorNr);
+  delay(color_Delay);
+}
+
 void InitPins()
 {
   Serial.println("INIT: Pins");
   Serial.println("READY: Pins");
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
 
